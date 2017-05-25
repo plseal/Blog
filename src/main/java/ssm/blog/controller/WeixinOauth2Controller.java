53b8797371d347f9facb74e5222675d2dabbd6a4
plseal <ttml0122@gmail.com>
2017-05-25 13:01:30 +0900

    change to log4j

--- src/main/java/ssm/blog/controller/WeixinOauth2Controller.java
+++ src/main/java/ssm/blog/controller/WeixinOauth2Controller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 
-import net.sf.json.JSONObject;
+
 import ssm.blog.entity.Blog;
 import ssm.blog.entity.BlogType;
 import ssm.blog.entity.PageBean;
@@ -32,6 +32,8 @@ import ssm.blog.util.PageUtil;
 import ssm.blog.util.StringUtil;
 import ssm.blog.util.WeixinUtil;
 
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
 /**
  * @Description Controller
  * @author songml
@@ -70,8 +72,8 @@ public class WeixinOauth2Controller {
 		
 		String str_code = request.getParameter("code");//我们要的code
 		logger.info("["+this.getClass()+"][get_code][CODE]"+str_code);
-		WeixinUtil wu = new WeixinUtil();
-		AccessToken accessToken = wu.get_oauth2_access_token_from_url(strAPPID, strAPPSECRET,str_code);
+		
+		AccessToken accessToken = get_oauth2_access_token_from_url(str_code);
 		logger.info("["+this.getClass()+"][get_code][openId]"+accessToken.getOpenid());
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("strResult", str_code);
@@ -88,5 +90,45 @@ public class WeixinOauth2Controller {
 		return modelAndView;
 
 	}
+	
+/**
+	 * 获取oauth2_access_token
+	 * 
+	 * @param appid 凭证
+	 * @param appsecret 密钥
+	 * @param CODE
+	 * @return
+	 */
+	private AccessToken get_oauth2_access_token_from_url( String code) {
+		
+		
+		logger.info("[WeixinUtil][get_oauth2_access_token_from_url][start]");
+		
+	// 获取oauth2_access_token的接口地址（GET） 限200（次/天）
+	    String oauth2_access_token_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
 
+		AccessToken accessToken = null;
+
+		String requestUrl = oauth2_access_token_url.replace("APPID", strAPPID).replace("APPSECRET", strAPPSECRET).replace("CODE", code);
+		logger.info("[WeixinUtil][get_oauth2_access_token_from_url][requestUrl]"+requestUrl);
+		JSONObject jsonObject = WeixinUtil.httpRequest(requestUrl, "GET", null);
+		// 如果请求成功
+		if (null != jsonObject) {
+			try {
+				accessToken = new AccessToken();
+				accessToken.setAccess_token(jsonObject.getString("access_token"));
+				accessToken.setOpenid(jsonObject.getString("openid"));
+				accessToken.setExpires_in(jsonObject.getInt("expires_in"));
+			} catch (JSONException e) {
+				accessToken = null;
+				// 获取token失败
+				logger.info("LINGZHU:获取token失败 errcode:{} errmsg:{}"+jsonObject.getInt("errcode")+jsonObject.getString("errmsg"));
+			}
+		}
+		logger.info("[WeixinUtil][get_oauth2_access_token_from_url][end]");
+		return accessToken;
+	}
+	
+	
+	
 }
