f0d257081e8ef9c965768f26d92fcbbdb4c7c86c
plseal <plseal@163.com>
2017-09-20 15:26:38 +0900

    qiniu yun qshell added

--- src/main/resources/tools/haiyu_backup.bat
+++ src/main/resources/tools/haiyu_backup.bat
@@ -2,6 +2,9 @@ rem *******************************Code Start*****************************
 @echo off
 
 set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%"
-D:\mysql\bin\mysqldump --opt -u root --password=123456 lingzhu t_bill > D:\db_backup\haiyu_t_bill_%Ymd%.sql
+set file_name=haiyu_t_bill_%Ymd%.sql
+D:\mysql\bin\mysqldump --opt -u root --password=123456 lingzhu t_bill > D:\db_backup\%file_name%
+rem *******************************DUMP OK*****************************
+D:\tools\qiniu\qshell fput myblog %file_name% D:\\db_backup\\%file_name%
 @echo on
 rem *******************************Code End*****************************
